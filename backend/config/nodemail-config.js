d69a14bc78ed40f6c8c276b8c594a2b7164308cb
TonC <ceton@tma.com.vn>
2024-07-19 16:26:41 +0700

    init commit

---
+++ backend/config/nodemail-config.js
@@ -0,0 +1,13 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, 
+    auth: {
+        user: '', 
+        pass: '',
+    },
+});
+
+export default transporter;
